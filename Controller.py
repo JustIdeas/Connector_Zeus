8abad7f4e4803e50327c6e812cc5903e2e5f14d0
JustIdeas <cardosoluke@gmail.com>
2018-01-17 00:48:58 -0200

    Firts version with API V1 integrated.
    Has functions to:
    
    Collect Uptime;
    Collect Channel;
    Collect average noise by channel;
    Collect average noise of all channels;
    Collect average noise of channel in use;
    Collect Firmware version;
    Collect number of clients connected.

---
+++ Controller.py
@@ -0,0 +1,94 @@
+import urllib3
+import sys
+import argparse
+
+from common import http_post
+
+urllib3.disable_warnings()
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Basic Functions')
+    parser.add_argument('-m', '--mode',
+                        help='Get act',
+                        required='True',
+                        default='version')
+    parser.add_argument('-ip', '--ip',
+                        help='ip address',
+                        default='127.0.0.1')
+    parser.add_argument('-p', '--port',
+                        help='https port',
+                        default='443')
+    parser.add_argument('-user', '--username',
+                        help='login username',
+                        default='something')
+    parser.add_argument('-pas', '--password',
+                        help='login password',
+                        default='something')
+    parser.add_argument('-ch', '--channel',
+                        help='login password',
+                        default='something')
+    results = parser.parse_args(args)
+    return (results.mode,
+            results.ip,
+            results.port,
+            results.username,
+            results.password,
+            results.channel
+            )
+
+
+def main():
+    m, ip, p, user, pas, ch  = check_arg(sys.argv[1:])
+
+    #print('mode:', m,'ip:', ip, 'port:', p, 'username:', user, 'password:', pas, 'channel:', ch)
+    if m == 'clients':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetClients()
+        if response['data']['clients'] == []:
+            print(0)
+        else:
+            print(len(response['data']['clients']))
+
+    if m == 'version':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetVersion()
+        if response["data"]["version"] == []:
+            print(0)
+        else:
+            print(response["data"]["version"])
+
+    if m == 'noise':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetNoise()
+        print(response)
+
+    if m == 'surveycount':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetNoise_channelCount()
+        print(response)
+
+    if m == 'noise_channel':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetNoise_byChannel()
+        print(response)
+
+    if m == 'noise_ownchannel':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetNoise_ownChannel()
+        print(response)
+
+    if m == 'channel':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).Getchannel()
+        print(response)
+
+    if m == 'uptime':
+        http_post.POST({'data': {'username': user, 'password': str(pas)}}, ip, p, user, pas, ch).login()
+        response = http_post.POST(0,ip, p, user, pas, ch).GetUptime()
+        print(response)
+main()
+
+
+
+
+
