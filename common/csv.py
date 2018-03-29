acebf89706950d76718326fd5ef30d401bfb257d
JustIdeas <cardosoluke@gmail.com>
2018-03-28 23:30:49 -0300

    Improving the CSV report when the info from database is not in the format of a list.

--- common/csv.py
+++ common/csv.py
@@ -10,45 +10,41 @@ class CSV:
 
 
     def construct(self):
-
-        #print("info hostID:", row[0], "info itemID:", row[1], "info value:", row[2], "info Clock:", row[3])
-        listI = self.info
-        #print (listI)
-        LHostId = []
-        LItemId = []
-        LOwner = []
-        LValue = []
-        LClock = []
-
-
-
-
-        for item in listI:
-           #print (item[2])
-
-           if item[2] != "0" and item[2].find("Traceback") == -1:
-                DictI = ast.literal_eval("{" + item[2] + "}")
-                for value, key in DictI.items():
-                    print("Owner:", value, "Value", key)
-
-                    LHostId.append(item[0])
-                    LItemId.append(item[1])
-                    LOwner.append(value)
-                    LValue.append(key)
-                    LClock.append(time.strftime("%D %H:%M", time.localtime(int(item[3]))))
-
-
-        zipList = zip(LHostId, LItemId, LOwner , LValue, LClock)
         try:
+            listI = self.info
+            LHostId = []
+            LItemId = []
+            LOwner = []
+            LValue = []
+            LClock = []
+
+
+
+
+            for item in listI:
+
+               if item[2] != "0" and item[2].find("Traceback") == -1 and ':' in item[2]:
+                    print(item[2])
+                    DictI = ast.literal_eval("{" + item[2] + "}")
+
+                    for value, key in DictI.items():
+                        print("Owner:", value, "Value", key)
+
+                        LHostId.append(item[0])
+                        LItemId.append(item[1])
+                        LOwner.append(value)
+                        LValue.append(key)
+                        LClock.append(time.strftime("%D %H"+"h", time.localtime(int(item[3]))))
+
+            zipList = zip(LHostId, LItemId, LOwner , LValue, LClock)
+            columns = ("HostID", "ItemID", "Fabricantes", "Qtd", "Data")
+            if columns is None:
+                return "Empty Columns"
             with open('names.csv', 'w') as csvfile:
-
                 writer = csv.writer(csvfile, delimiter=';', lineterminator='\n')
-
+                writer.writerow(columns)
                 for row in zipList:
-                    #print(Lowner, LValue)
                     writer.writerow(row)
-                # writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-                # writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
 
         except:
             print('error', sys.exc_info()[0], sys.exc_info()[1])
---
+++ common/w.py
@@ -0,0 +1,10 @@
+
+
+listi = ['Apple, Inc.', 'Samsung Electronics Co.,Ltd', 'Samsung Electronics Co.,Ltd', 'Apple, Inc.', 'LG Electronics (Mobile Communications)', 'SHENZHEN RF-LINK TECHNOLOGY CO.,LTD.']
+
+if ':' in listi[0]:
+    print("achou")
+
+else:
+    print("N Achou")
+    print(listi[0])
